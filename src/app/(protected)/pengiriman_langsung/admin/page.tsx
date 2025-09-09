777eca2cc3738bcb02651f73ffa2d34f23064c7d
Brama  Hendra Mahendra <jdoe@example.com>
2025-09-09 10:52:42 +0700

    menu daftar upload admin dan dinas. pengiriman langsung admin, dinas, pengawas.

--- src/app/(protected)/pengiriman_langsung/admin/page.tsx
+++ src/app/(protected)/pengiriman_langsung/admin/page.tsx
@@ -1,8 +1,5 @@
 "use client"
-import { Metadata } from "next";
-import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import MainPage from "@/components/pengirimanLangsung/FormPengirimanLangsungAdmin";
-
 import Breadcrumb from "@/components/breadcrumbs";
 
 const PengirimanLangsungAdmin = () => {
@@ -12,14 +9,12 @@ const PengirimanLangsungAdmin = () => {
   ];
 
   return (
-    // <DefaultLayout>
     <>
       <Breadcrumb breadcrumbs={breadcrumbs} />
       <div className="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
         <MainPage />
       </div>
     </>
-    // </DefaultLayout>
   );
 };
 
