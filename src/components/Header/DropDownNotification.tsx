6caf1aca95592f826a68caefdd283bb46db49743
arisafrianto2304 <arisafrianto23@gmail.com>
2025-09-28 22:47:43 +0700

    notifikasi header update

--- src/components/Header/DropDownNotification.tsx
+++ src/components/Header/DropDownNotification.tsx
@@ -274,7 +274,8 @@ const DropdownNotification = () => {
 
                       <span className="block">
                         <span className="block font-medium text-dark dark:text-white">
-                          {item.title}
+                          Pengiriman Langsung
+                          {/* {item.title} */}
                         </span>
                         <span className="block text-body-sm font-medium text-dark-5 dark:text-dark-6">
                           {item.subTitle}
